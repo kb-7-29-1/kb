1d32d6f538acbc64b91a337ccc6f559d9cc80bbb
hseooon <dks6979@naver.com>
2026-07-30 11:01:29 +0900

    feat: 경로 수정, jsp 관련 내용 삭제

--- backend/src/main/java/com/salgosipo/global/config/ServletConfig.java
+++ backend/src/main/java/com/salgosipo/global/config/ServletConfig.java
@@ -4,17 +4,23 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.multipart.MultipartResolver;
 import org.springframework.web.multipart.support.StandardServletMultipartResolver;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.*;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
 @EnableWebMvc
 @ComponentScan(basePackages = {
-        "com.salgosipo.controller",
-        "com.salgosipo.exception"})
+        "com.salgosipo.amenity.controller",
+        "com.salgosipo.auth.controller",
+        "com.salgosipo.bookmark.controller",
+        "com.salgosipo.comment.controller",
+        "com.salgosipo.destination.controller",
+        "com.salgosipo.onboarding.controller",
+        "com.salgosipo.property.controller",
+        "com.salgosipo.safety.controller",
+        "com.salgosipo.user.controller",
+        "com.salgosipo.global.exception"
+})
 public class ServletConfig implements WebMvcConfigurer {
     //스프링 내부에서 사용하는 서블릿(jsp)와 관련된 설정하는 파일.
 
@@ -26,22 +32,21 @@ public class ServletConfig implements WebMvcConfigurer {
     }
 
     @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/")
+                .setViewName("forward:/resources/index.html");
+    }
+
+    @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         //jsp에서 사용할 프론트용 자원들(js, css, img위치와 접근 주소 설정)
         registry
                 .addResourceHandler("/resources/**")
                 // url이 /resources/로 시작하는 모든 경로
                 .addResourceLocations("/resources/");
+
+        registry.addResourceHandler("/assets/**")
+                .addResourceLocations("/resources/assets/");
     }
     // webapp/resources/경로로 매핑
-
-    @Override
-    public void configureViewResolvers(ViewResolverRegistry registry) {
-        //서버의 결과를 넣을 jsp위치와 전체 경로를 결정
-        InternalResourceViewResolver bean = new InternalResourceViewResolver();
-        bean.setViewClass(JstlView.class);
-        bean.setPrefix("/WEB-INF/views/");
-        bean.setSuffix(".jsp");
-        registry.viewResolver(bean);
-    }
 }
--- backend/src/main/java/com/salgosipo/global/exception/CommonExceptionAdvice.java
+++ backend/src/main/java/com/salgosipo/global/exception/CommonExceptionAdvice.java
@@ -1,6 +1,7 @@
 package com.salgosipo.global.exception;
 
 import lombok.extern.log4j.Log4j2;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -11,21 +12,11 @@ import javax.servlet.http.HttpServletRequest;
 
 @ControllerAdvice
 @Log4j2
+@Order(1)
 public class CommonExceptionAdvice {
 
-    @ExceptionHandler(Exception.class)
-    public String except(Exception ex, Model model) {
-        log.error("Exception ......." + ex.getMessage());
-        model.addAttribute("exception", ex);
-        log.error(model);
-        return "error_page";
-    }
-
     @ExceptionHandler(NoHandlerFoundException.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    public String handle404(NoHandlerFoundException ex, Model model, HttpServletRequest request) {
-        log.error(ex);
-        model.addAttribute("uri", request.getRequestURI());
-        return "custom404";
+    public String handle404(NoHandlerFoundException ex) {
+        return "/resources/index.html";
     }
 }
