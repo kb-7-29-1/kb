479ac07da4fc16d1533a8027ef9f4780ce525c52
guhyeseong <ktw5214.10@gmail.com>
2026-07-31 16:31:08 +0900

    feat: 아이디 중복 체크 기능 구현

--- backend/src/main/java/com/salgosipo/global/security/config/SecurityConfig.java
+++ backend/src/main/java/com/salgosipo/global/security/config/SecurityConfig.java
@@ -116,6 +116,7 @@ public class SecurityConfig  extends WebSecurityConfigurerAdapter {
         http.authorizeRequests() // 경로별 접근권한 설정
                 .antMatchers(HttpMethod.OPTIONS).permitAll()
                 .antMatchers(HttpMethod.POST, "/api/user/signup", "/api/auth/login").permitAll() // 회원가입/로그인
+                .antMatchers(HttpMethod.GET, "/api/user/check-id").permitAll()
                 .antMatchers("/api/auth/**").permitAll()
                 .anyRequest().authenticated();
     }
