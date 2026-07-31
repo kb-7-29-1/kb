9e2d6ec49205b6d82dff11bab8184b561f7c5bb4
guhyeseong <ktw5214.10@gmail.com>
2026-07-31 15:33:02 +0900

    feat: 로그인/ 회원가입 백엔드 구현

--- backend/src/main/java/com/salgosipo/user/mapper/UserMapper.java
+++ backend/src/main/java/com/salgosipo/user/mapper/UserMapper.java
@@ -6,4 +6,10 @@ import org.apache.ibatis.annotations.Mapper;
 @Mapper
 public interface UserMapper {
     UserVO findByLoginId(String loginId);
+
+    void signup(UserVO user);
+
+    int countByLoginId(String loginId);
+
+    int countByEmail(String email);
 }
