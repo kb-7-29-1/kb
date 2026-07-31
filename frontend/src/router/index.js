69f3c1846d7da59998e9bf17e5f0108ea15cdc49
guhyeseong <ktw5214.10@gmail.com>
2026-07-31 11:31:46 +0900

    setting: router -> mypage 추가

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 import AuthPage from '../pages/auth/AuthPage.vue';
 import OnBoardingPage from '../pages/onboarding/OnBoardingPage.vue';
 import MapView from '../pages/home/MapView.vue';
+import MyPagePage from '../pages/mypage/MyPagePage.vue';
 
 const router = createRouter({
   history: createWebHistory(),
@@ -9,6 +10,7 @@ const router = createRouter({
     { path: '/', name: 'login', component: AuthPage },
     { path: '/onboarding', name: 'onboarding', component: OnBoardingPage },
     { path: '/map', name: 'map', component: MapView },
+    { path: '/mypage', name: 'mypage', component: MyPagePage },
   ],
 });
 
