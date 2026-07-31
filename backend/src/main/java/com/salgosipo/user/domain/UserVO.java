4ab4b7ec3554727475064a8d34d448bfcb755e64
guhyeseong <ktw5214.10@gmail.com>
2026-07-31 09:16:09 +0900

    setting: 회원가입 세팅 수정

--- backend/src/main/java/com/salgosipo/user/domain/UserVO.java
+++ backend/src/main/java/com/salgosipo/user/domain/UserVO.java
@@ -1,3 +1,21 @@
 package com.salgosipo.user.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class UserVO {
+    private Long userId;
+    private String loginId;
+    private String password;
+    private String name;
+    private String birthDate;
+    private String gender;
+    private String email;
+    private String delYn;
 }
--- backend/src/main/java/com/salgosipo/user/mapper/UserMapper.java
+++ backend/src/main/java/com/salgosipo/user/mapper/UserMapper.java
@@ -1,4 +1,9 @@
 package com.salgosipo.user.mapper;
 
+import com.salgosipo.user.domain.UserVO;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
 public interface UserMapper {
+    UserVO findByLoginId(String loginId);
 }
