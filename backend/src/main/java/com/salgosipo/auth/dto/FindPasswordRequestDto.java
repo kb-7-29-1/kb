f17488fbffc2ba4f653cf292813720be3b1e8e24
guhyeseong <ktw5214.10@gmail.com>
2026-08-02 15:41:47 +0900

    feat: 아이디/ 비밀번호 찾기 백엔드 구현

--- backend/src/main/java/com/salgosipo/auth/dto/FindPasswordRequestDto.java
+++ backend/src/main/java/com/salgosipo/auth/dto/FindPasswordRequestDto.java
@@ -1,4 +1,14 @@
 package com.salgosipo.auth.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class FindPasswordRequestDto {
+    private String loginId;
+    private String name;
+    private String email;
 }
--- backend/src/main/java/com/salgosipo/global/exception/ApiExceptionAdvice.java
+++ backend/src/main/java/com/salgosipo/global/exception/ApiExceptionAdvice.java
@@ -1,5 +1,6 @@
 package com.salgosipo.global.exception;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.servlet.NoHandlerFoundException;
 
 @RestControllerAdvice
 @Order(2)
+@Log4j2
 public class ApiExceptionAdvice {
 
     //404, 500 어노테이션별로 메서드 정의해서 설정
@@ -28,7 +30,7 @@ public class ApiExceptionAdvice {
 
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .header("Content-Type", "text/plain;charset=UTF-8")
-                .body("서버 내부 오류가 발생했습니다.");
+                .body("서버 내부 오류가 발생했습니다." + e.getMessage());
 
     }
 }
