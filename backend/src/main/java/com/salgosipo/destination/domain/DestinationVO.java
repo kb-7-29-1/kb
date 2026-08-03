ae3d128e1d0c49428a7436523566475be7fb9256
hseooon <dks6979@naver.com>
2026-08-03 10:12:11 +0900

    feat: 목적지 DB 저장

--- backend/src/main/java/com/salgosipo/destination/domain/DestinationVO.java
+++ backend/src/main/java/com/salgosipo/destination/domain/DestinationVO.java
@@ -1,4 +1,19 @@
 package com.salgosipo.destination.domain;
 
+import java.math.BigDecimal;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class DestinationVO {
+    private Integer destinationId;
+    private BigDecimal destLatitude;
+    private BigDecimal destLongitude;
+    private String destName;
+    private String destAddress;
 }
--- backend/src/main/java/com/salgosipo/onboarding/dto/OnboardingDTO.java
+++ backend/src/main/java/com/salgosipo/onboarding/dto/OnboardingDTO.java
@@ -1,4 +1,28 @@
 package com.salgosipo.onboarding.dto;
 
+import com.salgosipo.destination.dto.DestinationDTO;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class OnboardingDTO {
+    private Integer userId;
+    private Integer destinationId;
+    private String destinationType;
+    private String transportMode;
+    private Integer maxTravelTime;
+    private Integer budgetDeposit;
+    private Integer budgetRent;
+    private Integer minSafetyScore;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    private DestinationDTO destination;
 }
