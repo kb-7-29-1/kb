85fb88a3df35f44353f559c2e29af7c8590753a0
hseooon <dks6979@naver.com>
2026-08-03 14:18:11 +0900

    feat: 마이페이지 온보딩 패널 UI 구현

--- frontend/src/pages/mypage/MyPagePage.vue
+++ frontend/src/pages/mypage/MyPagePage.vue
@@ -1,8 +1,12 @@
 <script setup>
+import OnboardingPanel from '@/components/mypage/OnboardingPanel.vue';
 </script>
+
 <template>
   <main>
     <h1>마이페이지</h1>
     <RouterLink to="/map"> 지도로 돌아가기 </RouterLink>
+
+    <OnboardingPanel />
   </main>
 </template>
