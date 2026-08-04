39714821f0659b37ab5b20ca2e32aca8f240bd90
songenzhi <thd011128@gmail.com>
2026-08-04 14:28:33 +0900

    feat: 매물별 댓글 CRUD API 구현

--- backend/src/main/java/com/salgosipo/comment/domain/PropertyCommentVO.java
+++ backend/src/main/java/com/salgosipo/comment/domain/PropertyCommentVO.java
@@ -1,4 +1,17 @@
 package com.salgosipo.comment.domain;
 
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
 public class PropertyCommentVO {
+
+    private Long commentId;
+    private Long propertyId;
+    private Long userId;
+    private String content;
+    private String delYn;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 }
