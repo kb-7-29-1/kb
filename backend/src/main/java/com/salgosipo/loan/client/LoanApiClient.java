fbd05dce7c6ff77dc93b5572a20a209d05efc4a4
guhyeseong <ktw5214.10@gmail.com>
2026-08-04 15:48:22 +0900

    delete: 서민금융진흥원API 미사용 -> 삭제

--- backend/src/main/java/com/salgosipo/loan/client/LoanApiClient.java
+++ backend/src/main/java/com/salgosipo/loan/client/LoanApiClient.java
@@ -1,7 +1,6 @@
 package com.salgosipo.loan.client;
 
 import com.salgosipo.loan.dto.JeonseLoanApiResponse;
-import com.salgosipo.loan.dto.MinfundLoanApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -16,30 +15,11 @@ public class LoanApiClient {
 
     private final RestTemplate restTemplate;
 
-    @Value("${loan.minfund.api.key}")
-    private String serviceKey;
-
     @Value("${loan.jeonse.api.key}")
     private String jeonseApiKey;
 
-    private static final String MINFUND_END_POINT =
-            "https://apis.data.go.kr/B553701/LoanProductSearchingInfo/LoanProductSearchingInfo/getLoanProductSearchingInfo";
-
     private static final String JEONSE_END_POINT =
             "https://finlife.fss.or.kr/finlifeapi/rentHouseLoanProductsSearch.json";
-
-    // 서민금융진흥원 (월세용)
-    public MinfundLoanApiResponse fetchMinfundLoans() {
-         URI uri = UriComponentsBuilder.fromHttpUrl(MINFUND_END_POINT)
-                .queryParam("serviceKey", serviceKey)
-                .queryParam("pageNo", 1)
-                .queryParam("numOfRows", 50)
-                .queryParam("type", "xml")
-                .build(true) // 이미 인코딩된 값 인코딩 X
-                .toUri();
-
-        return restTemplate.getForObject(uri, MinfundLoanApiResponse.class);
-    }
 
     // 금감원 (전세용)
     public JeonseLoanApiResponse fetchJeonseLoans(String topFinGrpNo){
