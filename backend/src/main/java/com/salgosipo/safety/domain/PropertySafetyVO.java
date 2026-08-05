815c465e83c29f4c615b456449420cb64ce92067
김병승 <smartraynor@gmail.com>
2026-08-05 15:24:25 +0900

    feat: 점수 로직 진행 과정에서 필요한 데이터vo

--- backend/src/main/java/com/salgosipo/safety/domain/PropertySafetyVO.java
+++ backend/src/main/java/com/salgosipo/safety/domain/PropertySafetyVO.java
@@ -1,4 +1,81 @@
 package com.salgosipo.safety.domain;
 
+import java.time.LocalDateTime;
+
 public class PropertySafetyVO {
+    private Long propertyId;
+    private Integer destinationId;
+    private Integer safetyScore;
+    private Integer cctvCount;
+    private Integer streetLampCount;
+    private Boolean hasPoliceStation;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public PropertySafetyVO() {
+    }
+
+    public Long getPropertyId() {
+        return propertyId;
+    }
+
+    public void setPropertyId(Long propertyId) {
+        this.propertyId = propertyId;
+    }
+
+    public Integer getDestinationId() {
+        return destinationId;
+    }
+
+    public void setDestinationId(Integer destinationId) {
+        this.destinationId = destinationId;
+    }
+
+    public Integer getSafetyScore() {
+        return safetyScore;
+    }
+
+    public void setSafetyScore(Integer safetyScore) {
+        this.safetyScore = safetyScore;
+    }
+
+    public Integer getCctvCount() {
+        return cctvCount;
+    }
+
+    public void setCctvCount(Integer cctvCount) {
+        this.cctvCount = cctvCount;
+    }
+
+    public Integer getStreetLampCount() {
+        return streetLampCount;
+    }
+
+    public void setStreetLampCount(Integer streetLampCount) {
+        this.streetLampCount = streetLampCount;
+    }
+
+    public Boolean getHasPoliceStation() {
+        return hasPoliceStation;
+    }
+
+    public void setHasPoliceStation(Boolean hasPoliceStation) {
+        this.hasPoliceStation = hasPoliceStation;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
 }
