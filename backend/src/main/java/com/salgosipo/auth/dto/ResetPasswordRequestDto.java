d3d6f2361f974f52821824a54ea0ae141e2cc934
guhyeseong <ktw5214.10@gmail.com>
2026-08-05 12:08:59 +0900

    fix: 보안취약점 관련 코드 수정

--- backend/src/main/java/com/salgosipo/auth/dto/ResetPasswordRequestDto.java
+++ backend/src/main/java/com/salgosipo/auth/dto/ResetPasswordRequestDto.java
@@ -8,6 +8,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ResetPasswordRequestDto {
-    private Long userId;
+    private String resetToken;
     private String newPassword;
 }
