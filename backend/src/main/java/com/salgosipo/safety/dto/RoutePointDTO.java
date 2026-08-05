ecb5eae5dc68c4990b0916d840905bd8925f554b
김병승 <smartraynor@gmail.com>
2026-08-05 15:18:57 +0900

    feat: 감점,TMAP에서 받은 후보 경로 하나의 평가 결과, TMAP 보행 경로를 구성하는 개별 좌표, 안전api 최종응답, 전체를 담는 프론트 안전 api 요청 담는 각각의 dto

--- backend/src/main/java/com/salgosipo/safety/dto/RoutePointDTO.java
+++ backend/src/main/java/com/salgosipo/safety/dto/RoutePointDTO.java
@@ -1,4 +1,30 @@
 package com.salgosipo.safety.dto;
 
 public class RoutePointDTO {
+    private Double latitude;
+    private Double longitude;
+
+    public RoutePointDTO() {
+    }
+
+    public RoutePointDTO(Double latitude, Double longitude) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+
+    public Double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
+    }
 }
