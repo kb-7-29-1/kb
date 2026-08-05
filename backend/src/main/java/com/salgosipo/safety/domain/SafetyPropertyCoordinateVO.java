c88665edb7750bf92d7a8a89c475406338eb6aea
김병승 <smartraynor@gmail.com>
2026-08-05 15:24:25 +0900

    feat: 점수 로직 진행 과정에서 필요한 데이터vo

---
+++ backend/src/main/java/com/salgosipo/safety/domain/SafetyPropertyCoordinateVO.java
@@ -0,0 +1,43 @@
+package com.salgosipo.safety.domain;
+
+public class SafetyPropertyCoordinateVO {
+    private Long propertyId;
+    private String address;
+    private Double latitude;
+    private Double longitude;
+
+    public SafetyPropertyCoordinateVO() {
+    }
+
+    public Long getPropertyId() {
+        return propertyId;
+    }
+
+    public void setPropertyId(Long propertyId) {
+        this.propertyId = propertyId;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+
+    public Double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
+    }
+}
